fe74e0bc329dcc08d2d3b40309b8f9fa817a5bb9
trofimchuk <trofimchuk@itstep.academy>
2021-08-18 17:19:34 +0300

    CRUD Book

--- WebAPI-15/Controllers/BooksController.cs
+++ WebAPI-15/Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI_15.Data;
 using WebAPI_15.Data.Services;
 
 namespace WebAPI_15.Controllers
@@ -24,6 +25,27 @@ namespace WebAPI_15.Controllers
         {
             var allBooks = _bookService.GetAllBooks();
             return Ok(allBooks);
+        }
+
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody] Book book)
+        {
+            _bookService.AddBook(book);
+            return Ok();
+        }
+
+        [HttpPut("update-book/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody] Book book)
+        {
+            var updatedBook = _bookService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            _bookService.DeleteBook(id);
+            return Ok();
         }
     }
 }
--- WebAPI-15/Data/Services/BookService.cs
+++ WebAPI-15/Data/Services/BookService.cs
@@ -16,5 +16,43 @@ namespace WebAPI_15.Data.Services
         }
         public List<Book> GetAllBooks() => _context.Books.ToList();
 
+        public void AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        public Book UpdateBookById(int bookId, Book book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+
+            if(_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.DateRead;
+                _book.Author = book.Author;
+                _book.Rate = book.Rate;
+                _book.Genre = book.Genre;
+                _book.ImageURL = book.ImageURL;
+
+                _context.SaveChanges();
+            }
+
+            return _book;
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+
+            if(_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
+
     }
 }
